cee2d3e2e33ff28d0cad24d10a038eb257717ed5
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-04-29 00:15:39 -0300

    add: pagination feature to projects folder

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'apps.portfolio_app.apps.PortfolioAppConfig',
     'apps.user_app.apps.UserAppConfig',
 ]
