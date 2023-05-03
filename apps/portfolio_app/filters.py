f428d7ecc06c9db4b3f7df122235aaf8b4c97aff
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-05-03 19:50:55 -0300

    change: template and styling

--- apps/portfolio_app/filters.py
+++ apps/portfolio_app/filters.py
@@ -4,7 +4,7 @@ from django import forms
 from .models import *
 
 class PostFilter(django_filters.FilterSet):
-    headline = CharFilter(field_name='headline', lookup_expr='icontains', label='Headline')
+    headline = CharFilter(field_name='headline', lookup_expr='icontains', label="")
     tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget = forms.CheckboxSelectMultiple)
     class Meta:
         model = Post
--- setup/settings.py
+++ setup/settings.py
@@ -153,8 +153,8 @@ CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_CONFIGS = {
     'default': {
         'toolbar': 'full',
-        'height': 300,
-        'width': 900,
+        'height': 400,
+        'width': 1100,
     },
 }
 
