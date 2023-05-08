f72f5faa2184f81472a8786f261727411f5e84d8
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-05-08 13:55:46 -0300

    change: css design changes

--- apps/portfolio_app/filters.py
+++ apps/portfolio_app/filters.py
@@ -5,7 +5,7 @@ from .models import *
 
 class PostFilter(django_filters.FilterSet):
     headline = CharFilter(field_name='headline', lookup_expr='icontains', label="")
-    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget = forms.CheckboxSelectMultiple)
+    tags = django_filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(), widget = forms.CheckboxSelectMultiple, label = "")
     class Meta:
         model = Post
         fields = ['headline', 'tags']
